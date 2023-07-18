0be90add913945989a013dc72ab00b56e51cf67b
Nilesh darji <nilesh.28.2003@gmail.com>
2023-07-18 17:45:52 +0530

    add : og fetch

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const project = require('./routes/project');
 const comment = require('./routes/comment');
 const user = require('./routes/user');
 const review = require('./routes/review');
+const cheerio = require('cheerio');
 
 
 dotenv.config();
@@ -17,6 +18,28 @@ app.use('/api', comment);
 app.use('/api', user);
 app.use('/api', review);
 
+
+app.get('/fetch-opengraph', async (req, res) => {
+    const { link } = req.query;
+
+    try {
+        const response = await fetch(link);
+        const html = await response.text();
+        const $ = cheerio.load(html);
+
+        const ogDetails = {
+            title: $('meta[property="og:title"]').attr('content') || '',
+            image: $('meta[property="og:image"]').attr('content') || '',
+            description: $('meta[property="og:description"]').attr('content') || '',
+            url: $('meta[property="og:url"]').attr('content') || '',
+        };
+
+        res.json(ogDetails);
+    } catch (error) {
+        console.log('Error fetching Open Graph details:', error);
+        res.status(500).json({ error: 'Failed to fetch Open Graph details' });
+    }
+});
 
 
 const dbURL = process.env.MONGODB_URI || 'mongodb://localhost:27017/devCritique';
