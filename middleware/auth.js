155d097040a229827491183d5b4c65519f2805db
Nilesh darji <nilesh.28.2003@gmail.com>
2023-08-09 14:55:35 +0530

    fix:error

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,15 +3,15 @@ const jwt = require("jsonwebtoken");
 module.exports = async (request, response, next) => {
     try {
         //   get the token from the Authorization header
-        if (request.headers && request.headers['Authorization']) {
-            // console.log(request.headers['Authorization']); 
-            const decodedToken = await jwt.verify(request.headers['Authorization'], "RANDOM-TOKEN");
+        // console.log(request.headers['authorization']);
+        if (request.headers && request.headers['authorization']) {
+            const decodedToken = await jwt.verify(request.headers['authorization'], "RANDOM-TOKEN");
             request.user = decodedToken.id;
             next();
         } else {
-          response.status(401).json({
-            err:"you are not authorized"
-          })
+            response.status(401).json({
+                err: "you are not authorized"
+            })
         }
     } catch (error) {
         response.status(401).json({
